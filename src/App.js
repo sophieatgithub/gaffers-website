e32910cfb42fafd07d8bb9162fb54e8e28a94c6e
sophieatgithub <sophie@bajescu.com>
2019-11-27 11:07:46 +0000

    Font Awesome Library Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header'
 
 function App() {
--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,52 @@ export default class Home extends Component {
         return (
             <div className="component-container">
                 <header>
-                    <nav>
-                        <ul>
-                            <li>Home</li>
-                            <li>About</li>
-                            <li>Gigs</li>
-                            <li>Shop</li>
-                            <li>Gallery</li>
-                            <li>Contact</li>
-                            <li>Members</li>
+                    <nav className="navbar navbar-expand-lg bg-light fixed-top">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link navbar-brand">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    About
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    Gigs
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    Shop
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    Gallery
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    Contact
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link">
+                                    Members
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto nav-flex-icons">
+                            <li class="nav-item">
+                            <a class="btn btn-block btn-social btn-twitter">
+                                <span class="fa fa-twitter"></span> Sign in with Twitter
+                            </a>
+                            </li>
+                            <li class="nav-item">
+                            </li>
                         </ul>
                     </nav>
                     <img src={groupPhoto} alt="group of shanty singers" />
