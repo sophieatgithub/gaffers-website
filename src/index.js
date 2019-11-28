a7700ef20093ad886a5ccc86dde24012b68e4672
sophieatgithub <sophie@bajescu.com>
2019-11-28 10:32:43 +0000

    comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+//importing the libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+
+//
 import App from './App';
+
+//importing css styles
 import '../src/styles/font.css';
 
+
+//funtion 'render' from the library of ReactDOM
 ReactDOM.render((
     <HashRouter>
         <App />
